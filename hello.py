b4ff06e7a190732e4e6485b1e88a0f9f365bb929
Kojo Idrissa <kojo.idrissa@gmail.com>
2015-10-10 20:04:04 -0500

    Tried to go back to the original setup, to get things working again.

--- hello.py
+++ hello.py
@@ -5,4 +5,4 @@ app = Flask(__name__)
 
 @app.route('/')
 def hello():
-    return "Hello World! It's FLASK! And hopefully, updated Git!"
+    return "Hello World! It's FLASK! And hopefully, WORKING again!"
