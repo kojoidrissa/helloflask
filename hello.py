d36dc515297dcf65b931d60a3b1479c5fb26edc4
Kojo Idrissa <kojo.idrissa@gmail.com>
2015-10-10 20:06:35 -0500

    put Twilio reply code into hello.py

--- hello.py
+++ hello.py
@@ -1,8 +1,16 @@
-import os
-from flask import Flask
-
+#Should reply to an inbound text to my Twilio number 
+from flask import Flask, request, redirect
+import twilio.twiml
+ 
 app = Flask(__name__)
-
-@app.route('/')
-def hello():
-    return "Hello World! It's FLASK! And hopefully, WORKING again!"
+ 
+@app.route("/", methods=['GET', 'POST'])
+def hello_monkey():
+    """Respond to incoming calls with a simple text message."""
+ 
+    resp = twilio.twiml.Response()
+    resp.message("Hello, Mobile Monkey")
+    return str(resp)
+ 
+if __name__ == "__main__":
+    app.run(debug=True)
