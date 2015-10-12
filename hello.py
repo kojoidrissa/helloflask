e0005a21049091cb064799ed6a54c25d65a7ff88
Kojo Idrissa <kojo.idrissa@gmail.com>
2015-10-12 12:16:18 -0500

    Trying to fix indentation error. How DO I deal with a multiline variable assignment?

--- hello.py
+++ hello.py
@@ -20,7 +20,7 @@ def hello_monkey():
     from_number = request.values.get('From', None)
     if from_number in callers:
         message = callers[from_number] + ", thanks for the following message! "
-         + str(request.values.get('Body', None))
+        + str(request.values.get('Body', None))
     else:
         message = "Monkey, thanks for the message!"
 
