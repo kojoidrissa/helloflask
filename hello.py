381980252a8e58373ff607bb67d809195540582f
Kojo Idrissa <kojo.idrissa@gmail.com>
2015-10-12 10:01:10 -0500

    changed code to customize reply based on inbound number

--- hello.py
+++ hello.py
@@ -3,15 +3,27 @@ from flask import Flask, request, redirect
 import twilio.twiml
  
 app = Flask(__name__)
+
+# Try adding your own number to this list!
+callers = {
+    "+17137489037": "Kojo/殷可究",
+    "+12818448757": "Makiko",
+    "+12819281452": "Chanchan!",
+}
+
  
 @app.route("/", methods=['GET', 'POST'])
 def hello_monkey():
-    """Respond to incoming calls with a simple text message."""
+    """Respond & greet caller by name."""
  
+    from_number = request.values.get('From', None)
+    if from_number in callers:
+        message = callers[from_number] + ", thanks for the message!"
+    else:
+        message = "Monkey, thanks for the message!"
+
     resp = twilio.twiml.Response()
-    with resp.message("Hello, Mobile Monkey") as m:
-        m.media("https://demo.twilio.com/owl.png")
-    return str(resp)
+    resp.message(message)
  
 if __name__ == "__main__":
     app.run(debug=True)
