debac24ede6a417adfad5b0d5dc973cfea137e0c
Kojo Idrissa <kojo.idrissa@gmail.com>
2015-10-12 13:37:10 -0500

    changed code so ALL numbers will get their body in a response. But, it won't work until I upgrade my account

--- hello.py
+++ hello.py
@@ -7,6 +7,7 @@ import twilio.twiml
 app = Flask(__name__)
 
 # Try adding your own number to this list!
+# These other number won't work until I upgrade my account
 callers = {
     "+17137489037": "Kojo",
     "+12818448757": "Makiko",
@@ -22,7 +23,7 @@ def hello_monkey():
     if from_number in callers:
         message = callers[from_number] + ", thanks for " + sent_message
     else:
-        message = "Monkey, thanks for the message!"
+        message = "Monkey, thanks for sending me " + sent_message
 
     resp = twilio.twiml.Response()
     resp.message(message)
