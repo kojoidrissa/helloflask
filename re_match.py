a7b8f956600823889e437e0b2a99c8fac29dd694
Kojo Idrissa <kojo.idrissa@gmail.com>
2015-10-30 20:26:54 -0500

    added docstring to the function; updated test code

--- re_match.py
+++ re_match.py
@@ -1,21 +1,31 @@
 import re
 
 def input_valid(s):
+    '''
+    str --> str
+        if re.match: "Match found"
+        else: "No Match"
+    ''' 
+
     result = re.match(r'^\d?\d?[d,D]\d?\d$', s)
 
     if result == None:
-        print "No Match"
+        print ("No Match")
     else:
-        print "Match found"
+        print ("Match found")
 
 
 '''
 test cases
-input_valid('d8')
-input_valid('d10')
-input_valid('1d8')
-input_valid('1d10')
-input_valid('10d10')
+
+from re_match import input_valid as iv
+
+iv('d8')
+iv('d10')
+iv('1d8')
+iv('1d10')
+iv('10d10')
+
 '''
 
 # roll_list = ['d8', 'd10', '1d8', '1d10', '10d10', '10d8', '2d100']
