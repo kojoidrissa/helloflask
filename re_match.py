653c37b9c5124a0c1a3fd55326ad53e0d889400e
Kojo Idrissa <kojo.idrissa@gmail.com>
2015-11-08 13:58:14 -0600

    this should fix trailing 0 issue

--- re_match.py
+++ re_match.py
@@ -7,7 +7,7 @@ def input_valid(s):
         else: "No Match"
     ''' 
 
-    result = re.match(r'^\d?[1-9][d,D]\d\d?\d?$', s)
+    result = re.match(r'^\d?[1-9][d,D][1-9]\d?\d?$', s)
     return result
 
 
