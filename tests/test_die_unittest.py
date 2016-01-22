d93a6d2fa2d7b0a275e6c821b9f092b03f875ea5
Kojo Idrissa <kojo.idrissa@gmail.com>
2016-01-21 18:14:37 -0600

    CJ caught an error, I fixed it; https://github.com/kojoidrissa/helloflask/commit/230c9a9db39c5eead66314b04c0dcf9feb9e74a7#commitcomment-15590498

--- tests/test_die_unittest.py
+++ tests/test_die_unittest.py
@@ -14,8 +14,8 @@ from dice import Die
 
 class TestDie(unittest.TestCase):
     def test_die_roll_in_range(self):
-        die = Die(rnd.randint(1,100)) 
-        self.assertIn(die.roll(), range(1, die.sides))
+        die = Die(rnd.randint(1,101)) 
+        self.assertIn(die.roll(), range(1, die.sides+1))
 
 if __name__ == '__main__':
     unittest.main()
